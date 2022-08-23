4fd3e9ed7f52af91a6e7f61f93224bc3abd7bf9a
Daniel Ring <DanielKRing1@gmail.com>
2022-08-23 12:40:55 -0700

    Add types for deriving optional properties from existing types

--- global.d.ts
+++ global.d.ts
@@ -1,1 +1,3 @@
 export type Dict<T> = Record<string, T>;
+export type OptionalExceptFor<T, TRequired extends keyof T> = Partial<T> & Pick<T, TRequired>;
+export type RequiredExceptFor<T, TNotRequired extends keyof T> = Omit<T, TNotRequired> & Partial<Pick<T, TNotRequired>>;
