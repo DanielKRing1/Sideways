5db4a6d3db6e78ce5aa8df618fe62f0e65922c7e
Daniel Ring <DanielKRing1@gmail.com>
2022-08-22 17:26:23 -0700

    Confogure react-native-vector-icons to only include font-awesome icons in build

--- styled.d.ts
+++ styled.d.ts
@@ -11,6 +11,7 @@ declare module 'styled-components' {
             grayBorder: string;
 
             pastelPurple: string;
+            darkRed: string;
         };
         fonts: {
             main: string;
