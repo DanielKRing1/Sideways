d84d51064abf22962c43b12a812c8bd278b54983
Daniel Ring <DanielKRing1@gmail.com>
2022-09-22 12:52:50 -0700

    Create BoxShadow component

---
+++ components/Shadow/BoxShadow.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components/native";
+import { BoxShadowStyles } from "./BoxShadowStyles";
+
+export const BoxShadow = styled.View`
+    ${BoxShadowStyles}
+`;
---
+++ components/Shadow/BoxShadowStyles.ts
@@ -0,0 +1,9 @@
+export const BoxShadowStyles = `
+    elevation: 15;
+    backgroundColor: #000;
+
+    shadowOffset: {width: -10, height: -10};
+    shadowColor: black;
+    shadowOpacity: 0.5;
+    shadowRadius: 10;
+`;
