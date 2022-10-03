31b9eee1ddfb0d1bdf14ffc87ce1e5ea91a94320
Daniel Ring <DanielKRing1@gmail.com>
2022-10-03 10:44:49 -0700

    Complete HeatMap chart

---
+++ components/Charts/HeatMap/HeatMap.tsx
@@ -0,0 +1,53 @@
+import React, { FC, useMemo } from 'react';
+
+import { FlexRow } from '../../Flex';
+import { Dimensions } from '../types';
+
+type PartialHeatMapCell = {
+    color: string;
+    onPress: (index: number) => void;
+};
+export type CompleteHeatMapCell = {
+    index: number;
+} & PartialHeatMapCell;
+
+export type HeatMapCellProps = {
+    data: CompleteHeatMapCell;
+    gridDim: Dimensions;
+};
+
+export type HeatMapProps = {
+    CellComponent: FC<HeatMapCellProps>;
+    data: PartialHeatMapCell[];
+    cols: number;
+};
+const HeatMap: FC<HeatMapProps> = (props) => {
+    const { CellComponent, data, cols } = props;
+
+    const dataRows: CompleteHeatMapCell[][] = useMemo(() => data.reduce((acc, cur, i) => {
+        if (i % cols === 0) acc.push([]);
+        acc[acc.length - 1].push({
+            index: i,
+            ...cur,
+        });
+
+        return acc;
+    }, [])
+        , [data, cols]);
+
+    return (
+        <>
+            {
+                dataRows.map((row: CompleteHeatMapCell[], i) => (
+                    <FlexRow>
+                        {
+                            row.map((cell: CompleteHeatMapCell, i) => <CellComponent data={cell} gridDim={{ x: cols, y: data.length }} />)
+                        }
+                    </FlexRow>
+                ))
+            }
+        </>
+    );
+}
+
+export default HeatMap;
--- components/Charts/types.ts
+++ components/Charts/types.ts
@@ -2,3 +2,8 @@ export type DataPoint = {
     x: number | Date;
     y: number | Date;
 };
+
+export type Dimensions = {
+    x: number;
+    y: number;
+}
