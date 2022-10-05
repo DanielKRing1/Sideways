287e65a4ca18cd720f96fb13f8fd328a0aff6493
Daniel Ring <DanielKRing1@gmail.com>
2022-10-05 15:54:49 -0700

    Add timeseries stats reducer

--- redux/rootReducer.ts
+++ redux/rootReducer.ts
@@ -3,16 +3,22 @@ import createSidewaysSlice from './createSidewaysSlice';
 import readSidewaysSlice from './readSidewaysSlice';
 import rateSidewaysSlice from './rateSidewaysSlice';
 import deleteSidewaysSlice from './deleteSidewaysSlice';
+
+import snapshotCrudSlice from './snapshotCrudSlice';
+
 import identityStatsSlice from './identityStatsSlice';
 import recommendationStatsSlice from './recommendationStatsSlice';
-import snapshotCrudSlice from './snapshotCrudSlice';
+import timeSeriesStatsSlice from './timeSeriesStatsSlice';
 
 export default combineReducers({
     createSidewaysSlice,
     readSidewaysSlice,
     rateSidewaysSlice,
     deleteSidewaysSlice,
+    
+    snapshotCrudSlice,
+
     identityStatsSlice,
     recommendationStatsSlice,
-    snapshotCrudSlice,
+    timeSeriesStatsSlice,
 });
