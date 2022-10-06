e59211752f44638fd219cd5b479213f60dc688fa
Daniel Ring <DanielKRing1@gmail.com>
2022-10-06 13:47:02 -0700

    Conditionally render selected chart component

--- ssScreens/StackNav/TabView/AnayticsView/TimeSeriesScreen/index.tsx
+++ ssScreens/StackNav/TabView/AnayticsView/TimeSeriesScreen/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useMemo } from 'react';
 import { View } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 import styled, { useTheme } from 'styled-components/native';
@@ -7,6 +7,13 @@ import styled, { useTheme } from 'styled-components/native';
 import { AppDispatch, RootState } from '../../../../../ssRedux';
 import MyButton from '../../../../../ssComponents/ReactNative/MyButton';
 import MyText from '../../../../../ssComponents/ReactNative/MyText';
+
+// MY COMPONENTS
+import OutputLineGraph from './components/LineGraph';
+import OutputHistogram from './components/Histogram';
+import InputVenn from './components/Venn';
+import OutputHeatMap from './components/HeatMap';
+import { GRAPH_TYPES, HEAT_MAP, HISTOGRAM, LINE_GRAPH, VENN_PLOT } from 'ssRedux/timeSeriesStatsSlice';
 
 type GraphScreenProps = {
 
@@ -17,13 +24,28 @@ const GraphScreen: FC<GraphScreenProps> = (props) => {
     const theme = useTheme();
 
     // REDUX
+    const { activeSliceName, selectedGraph, readSSSignature, graphsSignature } = useSelector((state: RootState) => ({ ...state.readSidewaysSlice.toplevelReadReducer, ...state.readSidewaysSlice.toplevelReadReducer, ...state.timeSeriesStatsSlice }));
     const dispatch: AppDispatch = useDispatch();
-    const { activeSliceName, readSSSignature, graphsSignature } = useSelector((state: RootState) => ({ ...state.readSidewaysSlice.toplevelReadReducer, ...state.readSidewaysSlice.toplevelReadReducer, ...state.timeSeriesStatsSlice }));
+
+    // SELECTED CHART COMPONENT
+    const SelectedChart: FC<{}> = useMemo(() => {
+        switch(selectedGraph) {
+            case GRAPH_TYPES[LINE_GRAPH]:
+                return OutputLineGraph;
+            case GRAPH_TYPES[HISTOGRAM]:
+                return OutputHistogram;
+            case GRAPH_TYPES[VENN_PLOT]:
+                return InputVenn;
+            case GRAPH_TYPES[HEAT_MAP]:
+            default:
+                return OutputHeatMap;
+        }
+    }, [selectedGraph]);
 
     return (
         <View>
-
             
+            <SelectedChart/>
             
             <MyButton
                 style={{
