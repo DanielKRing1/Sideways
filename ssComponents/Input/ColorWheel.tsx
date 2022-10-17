063c32ef83b74ed5a0e1c15d48c5fb4ce594d0a4
Daniel Ring <DanielKRing1@gmail.com>
2022-10-17 14:12:06 -0700

    Create colorpicker component from @asianpersonn npm package

---
+++ ssComponents/Input/ColorWheel.tsx
@@ -0,0 +1,32 @@
+import React, { FC } from 'react';
+import { useWindowDimensions } from 'react-native';
+import ColorPicker from '@asianpersonn/rn-color-wheel';
+
+type MyColorPickerProps = {
+  handleColorChange: (color: string) => void;
+  handleColorSelected: (color: string) => void;
+};
+const MyColorPicker: FC<MyColorPickerProps> = (props) => {
+
+  const { handleColorChange, handleColorSelected } = props;
+
+  const { width, height } = useWindowDimensions();
+
+  return (
+    <ColorPicker
+      swatchesOnly={false}
+      onColorChange={handleColorChange}
+      onColorChangeComplete={handleColorSelected}
+      wheelRadius={width*7/8}
+      thumbSize={20}
+      sliderSize={20}
+      noSnap={true}
+      row={false}
+      swatchesLast={true}
+      swatches={true}
+      discrete={false}
+    />
+  )
+};
+
+export default MyColorPicker;
