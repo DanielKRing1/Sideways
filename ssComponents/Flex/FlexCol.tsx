b08b1263df5ebf8cfe9fc7107c7efd4fb7abc6dd
Daniel Ring <DanielKRing1@gmail.com>
2022-10-17 19:58:29 -0700

    Add style prop to flex components

--- ssComponents/Flex/FlexCol.tsx
+++ ssComponents/Flex/FlexCol.tsx
@@ -1,20 +1,23 @@
 import React, { FC, forwardRef } from 'react';
+import { ViewStyle, View } from 'react-native';
 
 import FlexBase from './FlexBase'
 
 type FlexColProps = {
+  style: ViewStyle;
   children: React.ReactNode;
 
   justifyContent?: 'flex-start' | 'flex-end' | 'center' | 'space-between' | 'space-around' | 'space-evenly';
   alignItems?: 'stretch' | 'flex-start' | 'flex-end' | 'center' | 'baseline';
 };
-const FlexCol: FC<FlexColProps> = forwardRef((props, ref) => {
-  const { children, justifyContent, alignItems } = props;
+const FlexCol: FC<FlexColProps> = forwardRef<View, FlexColProps>((props, ref) => {
+  const { style, children, justifyContent, alignItems } = props;
 
   return (
     <FlexBase
       // @ts-ignore
       ref={ref}
+      style={style}
       flexDirection='column'
       justifyContent={justifyContent}
       alignItems={alignItems}
--- ssComponents/Flex/FlexRow.tsx
+++ ssComponents/Flex/FlexRow.tsx
@@ -1,21 +1,24 @@
 import React, { FC, forwardRef } from 'react';
+import { ViewStyle } from 'react-native';
 import { View } from 'react-native';
 
 import FlexBase from './FlexBase'
 
 export type FlexRowProps = {
+  style?: ViewStyle;
   children: React.ReactNode;
 
   justifyContent?: 'flex-start' | 'flex-end' | 'center' | 'space-between' | 'space-around' | 'space-evenly';
   alignItems?: 'stretch' | 'flex-start' | 'flex-end' | 'center' | 'baseline';
 };
 const FlexRow: FC<FlexRowProps> = forwardRef<View, FlexRowProps>((props, ref) => {
-  const { children, justifyContent, alignItems } = props;
+  const { style, children, justifyContent, alignItems } = props;
 
   return (
     <FlexBase
-    // @ts-ignore
+      // @ts-ignore
       ref={ref}
+      style={style}
       flexDirection='row'
       justifyContent={justifyContent}
       alignItems={alignItems}
