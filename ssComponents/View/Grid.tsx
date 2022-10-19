218f3e4a38c2195ca07c8ebf625a63777d3174d3
Daniel Ring <DanielKRing1@gmail.com>
2022-10-18 17:04:00 -0700

    Create reusable Grid component to display an array of child nodes as rows of varying lengths

---
+++ ssComponents/View/Grid.tsx
@@ -0,0 +1,63 @@
+import React, { FC, useMemo } from 'react';
+import { View } from 'react-native';
+
+type GridProps = {
+  cols: number[];
+  children: React.ReactNode[];
+};
+
+const Grid: FC<GridProps> = (props) => {
+  const { cols, children } = props;
+
+  const childrenRows: React.ReactNode[] = useMemo(() => {
+    let rows: React.ReactNode[] = [];
+
+    let counter: number = 0;
+    while(counter < children!.length) {
+        // 1. Alternate column count
+        const colCountIndex: number = (rows.length%cols.length);
+        const curColCount: number = cols[colCountIndex];
+
+        // 2. Push row
+        rows.push(children!.slice(counter, counter+curColCount));
+
+        // 3. Increment counter
+        counter += curColCount;
+    }
+
+    return rows;
+  }, [children, cols]);
+
+  console.log(childrenRows);
+
+  return (
+    <View
+      style={{width: '100%'}}
+    >
+    {
+      childrenRows.map((row: React.ReactNode) => <FlexRow>{row}</FlexRow>)
+    }
+    </View>
+  );
+}
+
+export default Grid;
+
+type FlexRowProps = {
+  children?: React.ReactNode;
+}
+const FlexRow: FC<FlexRowProps> = (props) => {
+  const { children } = props;
+
+  return (
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent:'space-between',
+          alignItems:'center',
+        }}
+      >
+        {children}
+      </View>
+  );
+}
