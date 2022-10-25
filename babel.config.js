95391b81ef1602f5fc5dbf0e98f61fcd990e4dc9
Daniel Ring <DanielKRing1@gmail.com>
2022-10-25 15:23:37 -0700

    Try new  config

--- babel.config.js
+++ babel.config.js
@@ -4,5 +4,24 @@ module.exports = {
     'react-native-reanimated/plugin',
     // Includes only react-native-paper modules that are used
     'react-native-paper/babel',
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: {
+          'ssComponents/*': ['./ssComponents/*'],
+          'ssConstants/*': ['./ssConstants/*'],
+          'ssContexts/*': ['./ssContexts/*'],
+          'ssDatabase/*': ['./ssDatabase/*'],
+          'ssHooks/*': ['./ssHooks/*'],
+          'ssNavigation/*': ['./ssNavigation/*'],
+          'ssRealm/*': ['./ssRealm/*'],
+          'ssRedux/*': ['./ssRedux/*'],
+          'ssScreens/*': ['./ssScreens/*'],
+          'ssTheme/*': ['./ssTheme/*'],
+          'ssUtils/*': ['./ssUtils/*'],
+        },
+      },
+    ],
   ],
 };
