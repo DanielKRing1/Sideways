c4de3c3e2417259410c4d94d775b86b27043155e
Daniel Ring <DanielKRing1@gmail.com>
2022-10-28 16:55:25 -0700

    Create useTimeout hook to manage and clear a single 'setTimeout' at a time

---
+++ ssHooks/useTimeout.ts
@@ -0,0 +1,28 @@
+import {useEffect, useState} from 'react';
+
+export const useTimeout = () => {
+  const [to, setTO] = useState<number>();
+
+  // CLEAN UP
+  useEffect(() => {
+    // Clean up
+    return () => clearTO();
+  }, []);
+
+  const createTimeout = (cb: () => void, timeout: number) => {
+    // 1. Clear old timeout
+    clearTO();
+
+    // 2. Schedule and store new timeout
+    const id: number = setTimeout(cb, timeout);
+    setTO(id);
+  };
+
+  const clearTO = () => {
+    if (to !== undefined) clearTimeout(to);
+  };
+
+  return {
+    createTimeout,
+  };
+};
