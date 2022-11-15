c8e618db2fe5aeb08bd91a6d0c107a7414453706
Daniel Ring <DanielKRing1@gmail.com>
2022-11-14 21:02:10 -0800

    Fix NoInputsDisplay: get image to display

--- ssScreens/StackNav/TabNav/RateNav/RateHomeScreen/components/Input/List/NoInputsDisplay.tsx
+++ ssScreens/StackNav/TabNav/RateNav/RateHomeScreen/components/Input/List/NoInputsDisplay.tsx
@@ -1,23 +1,33 @@
 import React, {FC} from 'react';
-import {View} from 'react-native';
+import {View, useWindowDimensions} from 'react-native';
 
 import MyText from 'ssComponents/ReactNative/MyText';
 import CircleImage from 'ssComponents/Image/CircleImage';
-const NoInputsImage = require('ssImages/NoInputs');
+import AbsoluteView from 'ssComponents/View/AbsoluteView';
+const NoInputsImage = require('ssImages/NoInputs.jpg');
 
 type NoInputsDisplayProps = {};
 const NoInputsDisplay: FC<NoInputsDisplayProps> = props => {
+  const {width} = useWindowDimensions();
+
+  const diameter: number = width / 0.75;
+
   return (
     <View>
       <CircleImage
         source={NoInputsImage}
+        diameter={diameter}
         style={{
-          flex: 1,
-          resizeMode: 'cover',
-          alignItems: 'center',
+          alignSelf: 'center',
+          borderTopLeftRadius: 0,
+          borderTopRightRadius: 0,
+          borderBottomLeftRadius: 0,
+          borderBottomRightRadius: 0,
         }}
       />
-      <MyText>+ No Inputs</MyText>
+      <AbsoluteView>
+        <MyText>+ No Inputs</MyText>
+      </AbsoluteView>
     </View>
   );
 };
