603526a06f9e9a653217c08c750c59808f275d68
Daniel Ring <DanielKRing1@gmail.com>
2022-12-03 15:09:30 -0800

    Memoize NodeStats component + Replace placeholder icon with actual UserJsonMap input icon

--- ssScreens/StackNav/TabNav/AnayticsScreen/IdentityView/components/NodeStats.tsx
+++ ssScreens/StackNav/TabNav/AnayticsScreen/IdentityView/components/NodeStats.tsx
@@ -1,11 +1,15 @@
-import React, {FC} from 'react';
+import React, {FC, memo, useMemo} from 'react';
 import {View} from 'react-native';
+import {useSelector} from 'react-redux';
 import {DefaultTheme, useTheme} from 'styled-components/native';
 import {ID_KEY, RankedNode} from '@asianpersonn/realm-graph';
 
 import MyText from '../../../../../../ssComponents/ReactNative/MyText';
 import IconInput from '../../../../../../ssComponents/IconInput/IconInput';
 import {FlexCol} from '../../../../../../ssComponents/Flex';
+import {RootState} from 'ssRedux/index';
+import {cIdToCD, inToLastCId} from 'ssDatabase/hardware/realm/userJson/utils';
+import {GJ_CategoryDecoration} from 'ssDatabase/api/userJson/category/types';
 
 type NodeStatsProps = {
   nodeStats: RankedNode | undefined;
@@ -13,10 +17,30 @@ type NodeStatsProps = {
 const NodeStats: FC<NodeStatsProps> = props => {
   const {nodeStats} = props;
 
+  // THEME
   const theme: DefaultTheme = useTheme();
 
-  // TODO: Get Node icon info
-  const iconName: string = 'heart';
+  // REDUX
+  const {activeSliceName} = useSelector(
+    (state: RootState) => state.readSidewaysSlice.toplevelReadReducer,
+  );
+  const {fullUserJsonMap} = useSelector(
+    (state: RootState) => state.userJsonSlice,
+  );
+
+  const iconName: string = useMemo(() => {
+    // NOTE: If id becomes a uuid and is no longer the input name, then get the input name differently
+    const inputName: string = nodeStats !== undefined ? nodeStats.id : '';
+
+    const cId: string = inToLastCId(inputName, fullUserJsonMap);
+    const cd: GJ_CategoryDecoration = cIdToCD(
+      activeSliceName,
+      cId,
+      fullUserJsonMap,
+    );
+
+    return cd.icon;
+  }, [fullUserJsonMap]);
 
   return (
     <View>
@@ -30,7 +54,7 @@ const NodeStats: FC<NodeStatsProps> = props => {
             {Object.keys(nodeStats)
               .filter((key: string) => key !== ID_KEY)
               .map((key: string) => (
-                <MyText>
+                <MyText key={key}>
                   {key}: {nodeStats[key]}
                 </MyText>
               ))}
@@ -43,4 +67,4 @@ const NodeStats: FC<NodeStatsProps> = props => {
   );
 };
 
-export default NodeStats;
+export default memo(NodeStats);
