2b9ac66e1f672f7121a3a61af148ee22f860d6a8
Daniel Ring <DanielKRing1@gmail.com>
2022-12-08 02:23:42 -0800

    Disabled RatingButton if missing user inputs

--- ssScreens/StackNav/TabNav/RateNav/RateHomeScreen/components/RateButton.tsx
+++ ssScreens/StackNav/TabNav/RateNav/RateHomeScreen/components/RateButton.tsx
@@ -1,17 +1,20 @@
-import React, {FC} from 'react';
+import React, {FC, useMemo} from 'react';
 import {useWindowDimensions} from 'react-native';
-import {useDispatch} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import {DefaultTheme, useTheme} from 'styled-components/native';
 
 import MyButton from 'ssComponents/ReactNative/MyButton';
 import MyText from 'ssComponents/ReactNative/MyText';
-import {AppDispatch} from 'ssRedux/index';
+import {AppDispatch, RootState} from 'ssRedux/index';
 import {startRate} from 'ssRedux/rateSidewaysSlice';
 import {useTabBarHeight} from 'ssHooks/useTabBarHeight';
 
 type RateButtonProps = {};
 const RateButton: FC<RateButtonProps> = props => {
   // REDUX
+  const {inputs, outputs, rating} = useSelector(
+    (state: RootState) => state.rateSidewaysSlice,
+  );
   const dispatch: AppDispatch = useDispatch();
 
   // HANDLERS
@@ -24,8 +27,15 @@ const RateButton: FC<RateButtonProps> = props => {
   const {width, height} = useWindowDimensions();
   const {remainingHeight} = useTabBarHeight();
 
+  // LOCAL STATE
+  const validRating: boolean = useMemo(
+    () => inputs.length > 0 && outputs.length > 0 && rating > 0,
+    [inputs, outputs, rating],
+  );
+
   return (
     <MyButton
+      disabled={!validRating}
       style={{
         borderWidth: width / theme.border.widthDivisors.md,
         borderRadius: width / theme.border.radiusDivisors.sm,
