d3041b4aa0dd590ae1f4f3300be3f48da9863323
Daniel Ring <DanielKRing1@gmail.com>
2022-12-08 21:23:24 -0800

    Add margin/padding option to MyPadding component

--- ssComponents/ReactNative/MyBorder.tsx
+++ ssComponents/ReactNative/MyBorder.tsx
@@ -1,29 +1,73 @@
 import React, {FC} from 'react';
 import {View, ViewStyle, useWindowDimensions} from 'react-native';
 import {DefaultTheme, useTheme} from 'styled-components/native';
+
+import {DISPLAY_SIZE} from '../../global';
 import MyPadding from './MyPadding';
 
 type MyBorderProps = {
   children?: React.ReactNode;
   style?: ViewStyle;
+
+  marginBase?: DISPLAY_SIZE;
+  marginTop?: DISPLAY_SIZE;
+  marginRight?: DISPLAY_SIZE;
+  marginBottom?: DISPLAY_SIZE;
+  marginLeft?: DISPLAY_SIZE;
+
+  paddingBase?: DISPLAY_SIZE;
+  paddingTop?: DISPLAY_SIZE;
+  paddingRight?: DISPLAY_SIZE;
+  paddingBottom?: DISPLAY_SIZE;
+  paddingLeft?: DISPLAY_SIZE;
 };
 const MyBorder: FC<MyBorderProps> = props => {
-  const {children, style = {}} = props;
+  const {
+    children,
+    style = {},
+
+    marginBase,
+    marginTop,
+    marginRight,
+    marginBottom,
+    marginLeft,
+
+    paddingBase,
+    paddingTop,
+    paddingRight,
+    paddingBottom,
+    paddingLeft,
+  } = props;
 
   const theme: DefaultTheme = useTheme();
   const {width} = useWindowDimensions();
 
   return (
-    <View
-      style={{
-        borderWidth: width / theme.border.widthDivisors.sm,
-        borderColor: theme.border.color.main,
-        borderRadius: width / theme.border.radiusDivisors.sm,
+    <MyPadding
+      isMargin={true}
+      baseSize={marginBase}
+      topSize={marginTop}
+      bottomSize={marginBottom}
+      leftSize={marginLeft}
+      rightSize={marginRight}>
+      <View
+        style={{
+          borderWidth: width / theme.border.widthDivisors.sm,
+          borderColor: theme.border.color.main,
+          borderRadius: width / theme.border.radiusDivisors.sm,
 
-        ...style,
-      }}>
-      <MyPadding>{children}</MyPadding>
-    </View>
+          ...style,
+        }}>
+        <MyPadding
+          baseSize={paddingBase}
+          topSize={paddingTop}
+          bottomSize={paddingBottom}
+          leftSize={paddingLeft}
+          rightSize={paddingRight}>
+          {children}
+        </MyPadding>
+      </View>
+    </MyPadding>
   );
 };
 
