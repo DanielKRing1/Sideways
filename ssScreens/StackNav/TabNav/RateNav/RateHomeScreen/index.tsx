8c6150c54c3130d5628ae20f9da877aef50d7096
Daniel Ring <DanielKRing1@gmail.com>
2022-12-12 15:30:06 -0800

    Add 'RATING_TYPE' to RatingMenu

--- ssScreens/StackNav/TabNav/RateNav/RateHomeScreen/index.tsx
+++ ssScreens/StackNav/TabNav/RateNav/RateHomeScreen/index.tsx
@@ -1,10 +1,11 @@
 import React, {FC} from 'react';
 
 import RatingMenu from './components/RatingMenu/RatingMenu';
+import {RATING_TYPE} from './components/RatingMenu/types';
 
 type RateHomeScreenProps = {};
 const RateHomeScreen: FC<RateHomeScreenProps> = props => {
-  return <RatingMenu />;
+  return <RatingMenu ratingType={RATING_TYPE.Rate} />;
 };
 
 export default RateHomeScreen;
