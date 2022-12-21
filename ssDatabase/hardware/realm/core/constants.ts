81ff897323bf615eaec011add3612bc5d6b92318
Daniel Ring <DanielKRing1@gmail.com>
2022-12-21 02:10:07 -0800

    Add graph naming util

--- ssDatabase/hardware/realm/core/constants.ts
+++ ssDatabase/hardware/realm/core/constants.ts
@@ -1,3 +1,9 @@
-export const genInputGraphName = (sliceName: string) => `${sliceName}_INPUT`;
+const DELIM = '_';
+export const genInputGraphName = (sliceName: string) =>
+  `${sliceName}${DELIM}INPUT`;
 export const genCategoryGraphName = (sliceName: string) =>
-  `${sliceName}_CATEGORY`;
+  `${sliceName}${DELIM}CATEGORY`;
+export const getSliceNameFromGraphName = (graphName: string) => {
+  const end: number = graphName.lastIndexOf(DELIM);
+  return graphName.slice(0, end);
+};
