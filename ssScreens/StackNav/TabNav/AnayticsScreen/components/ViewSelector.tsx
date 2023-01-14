e919ef14bccd5a709b6a0353a2afb546c93a6ae6
Daniel Ring <DanielKRing1@gmail.com>
2023-01-13 17:10:10 -0800

    Style ViewSelector component

--- ssScreens/StackNav/TabNav/AnayticsScreen/components/ViewSelector.tsx
+++ ssScreens/StackNav/TabNav/AnayticsScreen/components/ViewSelector.tsx
@@ -1,30 +1,91 @@
 import React, {FC} from 'react';
-import {useDispatch} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 
+import {DISPLAY_SIZE} from '../../../../../global';
 import {FlexRow} from 'ssComponents/Flex';
 import MyButton from 'ssComponents/ReactNative/MyButton';
 import MyText from 'ssComponents/ReactNative/MyText';
 import {setSelectedView} from 'ssRedux/analyticsSlice';
-import {AppDispatch} from 'ssRedux/index';
+import {AppDispatch, RootState} from 'ssRedux/index';
 import {IdentityViewName, RecoViewName, TimeseriesViewName} from '../constants';
+import MyBorder from 'ssComponents/ReactNative/MyBorder';
+import {useTheme} from 'styled-components';
+import VerticalSpace from 'ssComponents/Spacing/VerticalSpace';
 
 type ViewSelectorProps = {};
 const ViewSelector: FC<ViewSelectorProps> = props => {
   // REDUX
+  const {selectedView} = useSelector(
+    (state: RootState) => state.analyticsSlice.selectedViewSlice,
+  );
   const dispatch: AppDispatch = useDispatch();
 
+  // THEME
+  const theme = useTheme();
+
   return (
-    <FlexRow>
-      <MyButton onPress={() => dispatch(setSelectedView(IdentityViewName))}>
-        <MyText>Identity</MyText>
-      </MyButton>
-      <MyButton onPress={() => dispatch(setSelectedView(RecoViewName))}>
-        <MyText>Recommendations</MyText>
-      </MyButton>
-      <MyButton onPress={() => dispatch(setSelectedView(TimeseriesViewName))}>
-        <MyText>Timeseries</MyText>
-      </MyButton>
-    </FlexRow>
+    <>
+      <VerticalSpace
+        spacing={20}
+        style={{
+          borderTopWidth: 2,
+          borderTopColor: theme.backgroundColors.accent,
+        }}
+      />
+
+      <FlexRow justifyContent="space-around">
+        <MyButton onPress={() => dispatch(setSelectedView(IdentityViewName))}>
+          <MyText
+            size={DISPLAY_SIZE.lg}
+            style={{
+              fontWeight: 'bold',
+              color:
+                selectedView === IdentityViewName
+                  ? theme.backgroundColors.accent
+                  : theme.colors.lightText,
+            }}>
+            You
+          </MyText>
+        </MyButton>
+
+        <MyButton onPress={() => dispatch(setSelectedView(RecoViewName))}>
+          <MyText
+            size={DISPLAY_SIZE.lg}
+            style={{
+              fontWeight: 'bold',
+              color:
+                selectedView === RecoViewName
+                  ? theme.backgroundColors.accent
+                  : theme.colors.lightText,
+            }}>
+            Recommend
+          </MyText>
+        </MyButton>
+
+        <MyButton onPress={() => dispatch(setSelectedView(TimeseriesViewName))}>
+          <MyText
+            size={DISPLAY_SIZE.lg}
+            style={{
+              fontWeight: 'bold',
+              color:
+                selectedView === TimeseriesViewName
+                  ? theme.backgroundColors.accent
+                  : theme.colors.lightText,
+            }}>
+            Over Time
+          </MyText>
+        </MyButton>
+      </FlexRow>
+
+      <VerticalSpace
+        spacing={20}
+        style={{
+          borderBottomWidth: 2,
+          borderBottomColor: theme.backgroundColors.accent,
+        }}
+      />
+      <VerticalSpace spacing={20} />
+    </>
   );
 };
 
