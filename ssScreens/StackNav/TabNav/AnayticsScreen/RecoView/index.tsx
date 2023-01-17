a76c7d092caffe9e490beedf85d0b4c254cc9c29
Daniel Ring <DanielKRing1@gmail.com>
2023-01-16 18:53:16 -0800

    Style Recommendation button

--- ssScreens/StackNav/TabNav/AnayticsScreen/RecoView/index.tsx
+++ ssScreens/StackNav/TabNav/AnayticsScreen/RecoView/index.tsx
@@ -1,9 +1,9 @@
 import React, {FC} from 'react';
-import {View} from 'react-native';
 import {useDispatch, useSelector} from 'react-redux';
 import {useTheme} from 'styled-components/native';
 
 // REDUX
+import {DISPLAY_SIZE} from '../../../../../global';
 import {AppDispatch, RootState} from '../../../../../ssRedux';
 import {startGetRecommendations} from '../../../../../ssRedux/analyticsSlice/recoStatsSlice';
 import GrowingRecoInputs from './components/GrowingRecoInputs';
@@ -12,6 +12,9 @@ import MyText from '../../../../../ssComponents/ReactNative/MyText';
 import RecommendationNodes from './components/RecommendationNodes';
 import StickyScrollView from 'ssComponents/View/StickyScrollView';
 import {GraphType} from 'ssDatabase/api/core/types';
+import MyBorder from 'ssComponents/ReactNative/MyBorder';
+import {FlexCol, FlexRow} from 'ssComponents/Flex';
+import {View} from 'react-native';
 
 type RecommendationScreenProps = {};
 const RecommendationScreen: FC<RecommendationScreenProps> = () => {
@@ -30,21 +33,40 @@ const RecommendationScreen: FC<RecommendationScreenProps> = () => {
   return (
     <StickyScrollView
       keyboardShouldPersistTaps="always"
-      stickyHeaderIndices={[1]}>
+      stickyHeaderIndices={[1]}
+      contentContainerStyle={{alignItems: 'center'}}>
       <GrowingRecoInputs />
 
-      <MyButton
+      <View
         style={{
-          width: '80%',
-          borderWidth: 1,
-          borderColor: theme.colors.grayBorder,
-          padding: 10,
-        }}
-        onPress={() =>
-          dispatch(startGetRecommendations({graphType: GraphType.Input}))
-        }>
-        <MyText>Get Recommendations!</MyText>
-      </MyButton>
+          height: '10%',
+        }}>
+        <MyBorder
+          shadow
+          paddingBase={DISPLAY_SIZE.md}
+          marginTop={DISPLAY_SIZE.sm}
+          marginBottom={DISPLAY_SIZE.sm}
+          style={{
+            backgroundColor: theme.backgroundColors.main,
+          }}>
+          <View
+            style={{
+              width: '100%',
+              alignItems: 'center',
+              backgroundColor: theme.backgroundColors.main,
+            }}>
+            <MyButton
+              style={{
+                backgroundColor: theme.backgroundColors.main,
+              }}
+              onPress={() =>
+                dispatch(startGetRecommendations({graphType: GraphType.Input}))
+              }>
+              <MyText>Get Recommendations!</MyText>
+            </MyButton>
+          </View>
+        </MyBorder>
+      </View>
 
       <RecommendationNodes />
     </StickyScrollView>
