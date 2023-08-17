8d4906f0cb751b62adee8faa643ce09f5d62a10f
Daniel Ring <DanielKRing1@gmail.com>
2023-08-17 16:03:40 -0700

    Rename root reducer's reducers

--- ssRedux/rootReducer.ts
+++ ssRedux/rootReducer.ts
@@ -1,13 +1,13 @@
 import {combineReducers} from 'redux';
 
-import analyticsReducers from 'newRedux/analytics';
-import appStateReducers from 'newRedux/appState';
-import fetchedReducers from 'newRedux/fetched';
-import inputReducers from 'newRedux/input';
+import analytics from 'newRedux/analytics';
+import appState from 'newRedux/appState';
+import fetched from 'newRedux/fetched';
+import input from 'newRedux/input';
 
 export default combineReducers({
-  analyticsReducers,
-  appStateReducers,
-  fetchedReducers,
-  inputReducers,
+  analytics,
+  appState,
+  fetched,
+  input,
 });
